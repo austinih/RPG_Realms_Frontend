bdf0197e2728cc0524b76b263b786939ef6f9ace
Austin <austinih46@gmail.com>
2023-04-10 16:48:26 -0500

    Header CSS

--- rpg_realms/src/components/Nav.jsx
+++ rpg_realms/src/components/Nav.jsx
@@ -1,26 +1,31 @@
 import { BsFillLightningFill, BsGearFill } from 'react-icons/bs'
-import { FaHome,FaPlus, FaFire, FaUserPlus,FaUser } from 'react-icons/fa'
+import { FaSignInAlt,FaSignOutAlt,FaPlus, FaFire, FaUserPlus,FaUser } from 'react-icons/fa'
 import { Link } from "react-router-dom"
 
 export default function NavBar() {
     return(
        <div>
         {/* <h1 className='text-3xl'>RPG Realms</h1> */}
-        <div className=" w-auto flex flex-row justify-evenly " >
+        <div className=" w-auto flex flex-row justify-evenly mr-3 " >
             
+            <Link to='/login'>
+                <NavBarIcon icon={<FaSignInAlt size="28" />} text={"Log In"}/>
+            </Link>
+            <Link to='/register'>
+                <NavBarIcon icon={<FaUserPlus size="28" />} text={"Register"}/>
+            </Link>
             <Link to='/createreview'>
                 <NavBarIcon icon={<FaPlus size="28" />} text={"New Review"}/>
             </Link>
-            <Link to='/'>
-                <NavBarIcon icon={<BsGearFill size="28" />} text={"Settings"}/>
+            {/* <Link to='/'>
+                <NavBarIcon icon={<FaFire size="28" />} text={"Trending"}/>
+            </Link> */}
+            <Link to='/profile'>
+                <NavBarIcon icon={<FaUser size="28" />} text={"Profile"}/>
             </Link>
-            <Link to='/'>
-                <NavBarIcon icon={<FaFire size="28" />} />
-            </Link>
-            <Link to='/'>
-                <NavBarIcon icon={<FaUser size="28" />} />
-            </Link><Link to='/'>
-                <NavBarIcon icon={<FaUserPlus size="28" />} />
+            
+            <Link to='/logout'>
+                <NavBarIcon icon={<FaSignOutAlt size="28" />} text={"Log Out"}/>
             </Link>
             
         </div>

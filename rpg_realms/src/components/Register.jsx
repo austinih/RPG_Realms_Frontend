8bc96dc089f969f3802e4371ea46760270dd00db
Austin <austinih46@gmail.com>
2023-04-11 09:47:36 -0500

    register Component Outline

--- rpg_realms/src/components/Register.jsx
+++ rpg_realms/src/components/Register.jsx
@@ -1,8 +1,111 @@
+import { useNavigate } from "react-router"
+import { useState } from "react"
+
 export default function Register() {
+    
+    let navigate = useNavigate()
+
+    const [formValues, setFormValues] = useState({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+      }
+    
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault()
+    //     await RegisterUser({
+    //       name: formValues.name,
+    //       username: formValues.username,
+    //       email: formValues.email,
+    //       password: formValues.password
+    //     })
+    //     setFormValues({
+    //       name: '',
+    //       email: '',
+    //       password: '',
+    //       confirmPassword: ''
+    //     })
+    //     navigate('/signin')
+    //   }
+
+
     return(
        
-       <div className="home-container">
-            <h1>Register</h1>
+        <div className="w-72 m-auto " >
+            <h1 className="text-3xl text-center mb-3">Register</h1>
+            <form className="w-72 h-[27rem] p-4  shadow-2xl">
+                <div>
+                    <label htmlFor="name" className="flex">Name:</label>
+                    <input className="input-field"
+                        onChange={handleChange}
+                        name="name"
+                        type="text"
+                        placeholder="name"
+                        value={formValues.name}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="username" className="flex">Username:</label>
+                    <input className="input-field"
+                        onChange={handleChange}
+                        name="username"
+                        type="text"
+                        placeholder="username"
+                        value={formValues.username}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email" className="flex">Email:</label>
+                    <input className="input-field"
+                        onChange={handleChange}
+                        name="email"
+                        type="text"
+                        placeholder="email"
+                        value={formValues.email}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password" className="flex">Password:</label>
+                    <input className="input-field"
+                        onChange={handleChange}
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        value={formValues.password}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirmPassword" className="flex">Confirm Password:</label>
+                    <input className="input-field"
+                        onChange={handleChange}
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="confirm password"
+                        value={formValues.confirmPassword}
+                        required
+                    />
+                </div>
+                <button className="mt-2 px-3 py-1 rounded-3xl float-right bg-tertiary text-white"
+                    disabled={
+                        !formValues.email ||
+                        (!formValues.password &&
+                            formValues.confirmPassword === formValues.password)
+                    }>Sign up
+                </button>
+
+
+            </form>
+
             
             
         </div>

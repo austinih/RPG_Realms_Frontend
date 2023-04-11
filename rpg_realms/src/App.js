cfeca03dc3574b94f68e45808bbc70799c34ee6e
Austin <austinih46@gmail.com>
2023-04-10 21:54:17 -0500

    Featured Game

--- rpg_realms/src/App.js
+++ rpg_realms/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
+import Home from './components/home/Home';
 import Login from './components/LogIn';
 import {Routes, Route} from 'react-router-dom'
 import Register from './components/Register';

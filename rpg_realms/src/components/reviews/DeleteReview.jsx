4119765eacf49a6db248001b2bdcf3cf1fa23794
Austin <austinih46@gmail.com>
2023-04-14 13:49:47 -0500

    delete complete

--- rpg_realms/src/components/reviews/DeleteReview.jsx
+++ rpg_realms/src/components/reviews/DeleteReview.jsx
@@ -9,17 +9,34 @@ export default function DeleteReview() {
         const response = await axios.delete(
             `http://localhost:8000/delete-review/${reviewId}/`,
         )
+        alert("You have successfully deleted this review")
         navigate(`/profile/`)
 
     };
 
     return (
-        <div className="w-80 m-auto  bg-white shadow-md shadow-slate-300 p-5">
-            <p className="text-black">Are you sure you want to delete your review?</p>
-            <button onClick={deleteSelectedReview} className="buttons">Yes</button>
-            <Link to={`/profile`}>
-                <button className="buttons">no</button>
-            </Link>
+        <div className="flex align-middle">
+            <div className=" w-96 h-44 m-auto  bg-white shadow-md shadow-slate-300 p-5">
+                <p className="text-black text-center">Are you sure you want to permanently delete your review?</p>
+                <div className="flex justify-center mt-5">
+                    <button onClick={deleteSelectedReview} className="buttons mx-3 hover:bg-red-400">
+                        Yes 
+                        <br></br>
+                        <span className="text-xs"> 
+                            Delete this review
+                        </span>
+                    </button>
+                    <Link to={`/profile`}>
+                        <button className="buttons mx-3">
+                            No
+                            <br></br>
+                            <span className="text-xs">
+                                Return to Profile
+                            </span>
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
   }

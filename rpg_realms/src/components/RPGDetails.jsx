ff9fcfc8c68e535aba11f50a2be14f0e704d9058
Austin <austinih46@gmail.com>
2023-04-13 21:34:13 -0500

    update

--- rpg_realms/src/components/RPGDetails.jsx
+++ rpg_realms/src/components/RPGDetails.jsx
@@ -45,6 +45,7 @@ export default function RPGDetails() {
     return rpg && publisher ? (
        
        <div >
+{/* RPG Description */}
             <div className="p4 flex justify-center">
                 <img src={rpg.image_url} className="max-w-xs max-h-[65vh]"></img>
                 <div className="ml-5 w-[50vw]">
@@ -57,30 +58,34 @@ export default function RPGDetails() {
                     </div>
                 </div>
             </div>
+{/* Publisher Information */}
             <p>Publisher</p>
             <div>
                 <p>{publisher.name}</p>
             </div>
-            <div className="my-5 pb-10 w-[75vw] mx-auto">
-                <div className="ml-12 py-1 w-44 bg-primary text-white  text-xl rounded-t-2xl  text-center">Reviews</div>
-                <div className=" mx-8  flex self-center flex-col  px-8  bg-white">
+{/* Reviews */}
+            <div className="my-5 pb-10 w-[75vw] mx-auto ">
+                <div className="ml-12 py-1 w-44 bg-primary text-white  text-xl rounded-t-2xl  text-center ">Reviews</div>
+                <div className=" mx-8  flex self-center flex-col  px-8  bg-white ">
+                    <div className=" p-2y my-2 mx-3 text-primary font-bold flex flex-row justify-around  place-items-center text-center underline">
+                        <p className="-ml-2 w-16 text-center">Score </p>
+                        <p className="w-[22rem] pl-5 "> Title  </p>
+                        <p className="w-32 text-center">User </p>
+                        <p className="w-40 text-center">Date Posted</p> 
+                    </div>
                     {rpg.reviews ? rpg.reviews.map((review) => (
                         <div>
                             <details className=" py-2 my-2 mx-3 hover:border-2 hover:border-primary" >
                                 <summary className="
                                         flex flex-row justify-around  place-items-center  cursor-pointer hover:rounded-md border-collapse ">
-                                    <p className="w-10 text-center">{review.score} </p>
+                                    <p className="w-16 text-center">{review.score} </p>
                                     <p className="text-2xl">|</p>
                                     <p className="w-[22rem] pl-5 ">{review.title}  </p>
                                     {/* <p>{review.content} |</p> */}
                                     <p className="text-2xl ">|</p>
                                     <p className="w-32 text-center">User </p>
                                     <p className="text-2xl">|</p>
-                                    <p className="w-40 text-center">Date Posted </p>                         
-                                    
-                                    {/* <Link>
-                                        <button className="buttons w-28 align-middle open:bg-amber-50" >Read</button>
-                                    </Link> */}
+                                    <p className="w-40 text-center">{review.date_posted} </p>                         
                                 </summary>
                                     {/* https://www.kindacode.com/article/tailwind-css-how-to-create-accordions-collapsible-content/ */}
                                 <div className="mx-5 mt-3 p-5  border-2 border-primary">
@@ -96,6 +101,7 @@ export default function RPGDetails() {
                         
                     )): <h2>No reviews found</h2>
                     }
+{/* Create Review Button */}
                     <button className="mt-2 rounded-t-2xl w-56 mx-auto bg-secondary text-primary  hover:text-white hover:h-10 hover:w-96 transition-all duration-300 ease-linear hover:font-bold hover:text-lg hover:rounded-t-md ">+ Review</button>
 
                 </div>
